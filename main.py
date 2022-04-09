1823a73bcba04e67e1e1e64caf6d515537291ff1
Taccetta <n.taccetta@alumno.um.edu.ar>
2022-04-09 09:36:14 -0300

    improve pause exit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from tateti import Tateti
+import time
 
 if __name__ == '__main__':
 
     print ("\n\n(press enter to use default name)")
     iniciate = Tateti(str(input("Insert your name: ")))
     iniciate.game()
+    time.sleep(1)
+    input("\n\nPress enter to exit...")
