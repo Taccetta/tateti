95cafffabdf3cf56b64ef517a23bbc16088eacf1
Taccetta <n.taccetta@alumno.um.edu.ar>
2022-04-09 09:30:41 -0300

    naming

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from tateti import Tateti
 
 if __name__ == '__main__':
 
-    iniciate = Tateti(str(input("Insert your name: \n(press enter to use default name)")))
+    print ("\n\n(press enter to use default name)")
+    iniciate = Tateti(str(input("Insert your name: ")))
     iniciate.game()
